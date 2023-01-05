8ac1b430ea5e13e081560d581f9649c6e0890454
Mateus Henrique <mateushcandido5@gmail.com>
2023-01-05 17:35:58 -0300

    Criação da entidade LivroCaixa

---
+++ src/main/java/com/mtzz/entities/LivroCaixa.java
@@ -0,0 +1,46 @@
+package com.mtzz.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "LIVRO_CAIXA")
+public class LivroCaixa implements Serializable {
+    public static final long serializable = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy", timezone = "GMT")
+    @Column(nullable = false)
+    private Date dataLancamento;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false, length = 1)
+    private String tipo; //D-Débito ou C-Crédito
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private Double valor;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+}
