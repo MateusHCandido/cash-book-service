493a096b7f75a0c564d6032b65fe482ec310446f
Mateus Henrique <mateushcandido5@gmail.com>
2023-01-05 17:37:59 -0300

    Criação do repositório para LivroCaixa

---
+++ src/main/java/com/mtzz/repositories/LivroCaixaRepository.java
@@ -0,0 +1,11 @@
+package com.mtzz.repositories;
+
+import com.mtzz.entities.LivroCaixa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LivroCaixaRepository extends JpaRepository<LivroCaixa, Long> {
+
+
+}
